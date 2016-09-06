5cdbdbc26bdb9127d3ad9286e618a2fb2ea93b2f
Darren <Darren.sz@flexsystem.com>
2016-09-06 11:31:09 +0800

    添加项目
    添加项目所需的资源

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+//引入css
+require("../less/global");
+require("../less/index");
+
+$('body').append('这是js动态生成的内容：这是index页面！')
+
---
+++ src/js/list.js
@@ -0,0 +1,6 @@
+//引入css
+require("../less/global");
+require("../less/list");
+
+$('body').append('这是js动态生成的内容，这是list页面！')
+
